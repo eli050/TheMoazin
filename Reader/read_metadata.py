ff06bfbbc4bd3dcbaaf26fbe77fde8d029bd5a7b
eli <lykkh78@gmail.com>
2025-09-07 12:00:27 +0300

    deleted data folder and adding read metadata class

---
+++ Reader/read_metadata.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import os
+from pprint import pprint
+
+class ReadMetaData:
+    def __init__(self, folder_path:str):
+        self.folder_path = folder_path
+    @staticmethod
+    def _get_meta_data(file_path:str):
+        file_path = Path(file_path)
+        file_stat = file_path.stat()
+        meta_data = dict()
+        meta_data["file_path"] = file_path
+        meta_data["file_size"] = file_stat.st_size
+        meta_data["create_time"] = file_stat.st_ctime
+        meta_data["file_name"] = file_path.name
+        return meta_data
+    def read_folder(self):
+        list_of_metadata = list()
+        for file_name in os.listdir(self.folder_path):
+            file_path = self.folder_path+"\\"+file_name
+            list_of_metadata.append(ReadMetaData._get_meta_data(file_path))
+        return list_of_metadata
+
+
+
+
+
+
+# rmt = ReadMetaData("C:\\podcasts")
+# pprint(rmt.read_folder())
