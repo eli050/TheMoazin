f58a74940572f43e7d385120d2a6f5ff07d0b09a
eli <lykkh78@gmail.com>
2025-09-09 16:59:01 +0300

    changing decode_base function

--- services/processor/utils.py
+++ services/processor/utils.py
@@ -2,7 +2,7 @@ import base64
 
 
 
-def encode_base64(base64_string:str, format_return:str = "ascii" ) -> list:
+def decode_base64(base64_string:str, format_return:str = "ascii" ) -> list:
     decoded_bytes = base64.b64decode(base64_string)
     decoded_string = decoded_bytes.decode(format_return)
     return decoded_string.split(",")
