12fb6345eb249c07d23ad59af84b055b007a18b9
eli <lykkh78@gmail.com>
2025-09-10 10:10:53 +0300

    I added the logic in functions in the Classification class

--- services/processor/classification.py
+++ services/processor/classification.py
@@ -1,6 +1,6 @@
 from utils import decode_base64
 from config import MOST_DANGEROUS_WORDS, DANGEROUS_WORDS
-import re
+
 
 
 class Classification:
@@ -8,19 +8,50 @@ class Classification:
         self.text_to_check = text_to_check
         self.list_most_dangerous_words:list[str] = decode_base64(MOST_DANGEROUS_WORDS)
         self.list_dangerous_words:list[str] = decode_base64(DANGEROUS_WORDS)
-    def calculate_danger(self):
-        danger_count = 0
-        for word in self.list_dangerous_words:
-            if re.search(r'\b' + re.escape(word) + r'\b',self.text_to_check):
-                danger_count += 1
+    def classify(self) -> dict:
+        avg_danger = self._calculate_danger()
+        danger_level = Classification._danger_levels(avg_danger)
+        is_danger = Classification._is_bds(avg_danger)
+        return {
+            "bds_percent": avg_danger,
+            "bds_threat_level": danger_level,
+            "is_bds": is_danger
+        }
+    def _calculate_danger(self):
+        frequency = 0
+        list_of_text = self.text_to_check.lower().strip().split()
+        words_pair_count = 0
+        for i, word in enumerate(list_of_text):
+            if word in self.list_dangerous_words:
+                # print(word)
+                frequency += 1
+            elif word in self.list_most_dangerous_words:
+                # print(word)
+                frequency += 2
+            if i + 1 < len(list_of_text):
+                if (f"{list_of_text[i]} {list_of_text[i+1]}"
+                        in self.list_dangerous_words):
+                    # print(f"{list_of_text[i]} {list_of_text[i+1]}")
+                    frequency += 1
+                    words_pair_count += 1
+                elif (f"{list_of_text[i]} {list_of_text[i + 1]}"
+                        in self.list_most_dangerous_words):
+                    # print(f"{list_of_text[i]} {list_of_text[i + 1]}")
+                    frequency += 2
+                    words_pair_count += 1
+        return frequency / (len(list_of_text) - words_pair_count)
 
-        for word in self.list_most_dangerous_words:
-            if re.search(r'\b' + re.escape(word) + r'\b',self.text_to_check):
-                danger_count += 2
-        return (danger_count* 100)/ len(self.text_to_check.split())
+    @staticmethod
+    def _is_bds( num_of_dangerous:float):
+        return num_of_dangerous > 0.7
 
-    def is_bds(self):
-        pass
-    def danger_levels(self):
-        pass
+    @staticmethod
+    def _danger_levels(num_of_dangerous):
+        if num_of_dangerous < 0.30:
+            return "none"
+        elif 0.30 <= num_of_dangerous <= 0.7:
+            return "medium"
+        else:
+            return "high"
+
 
--- services/processor/utils.py
+++ services/processor/utils.py
@@ -5,4 +5,4 @@ import base64
 def decode_base64(base64_string:str, format_return:str = "ascii" ) -> list:
     decoded_bytes = base64.b64decode(base64_string)
     decoded_string = decoded_bytes.decode(format_return)
-    return decoded_string.split(",")
+    return decoded_string.lower().split(",")
